e1701e0e69ce23628654cd39cbd7708c19305b2a
manastyagi <manasvi.tyagi@gmail.com>
2020-10-10 05:28:13 +1300

    clean up further

--- app/src/main/java/com/manas/a2p3/MainActivity.java
+++ app/src/main/java/com/manas/a2p3/MainActivity.java
@@ -1,38 +1,25 @@
 package com.manas.a2p3;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     Button getLocation;
     private TextView latitudeField;
     private TextView longitudeField;
